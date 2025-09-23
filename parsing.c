afeb8c704d8d68b658f97c5adca342a4212a59c6
Raffaele Lungarella <lungarellaraffaele@gmail.com>
2025-09-23 19:04:19 +0200

    Init

---
+++ parsing.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef _WIN32
+#include <string.h>
+#include "mpc.h"
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline(char* prompt) {
+    fputs(prompt, stdout);
+    fgets(buffer, 2048, stdin);
+    char* cpy = malloc(strlen(buffer) + 1);
+    strcpy(cpy, buffer);
+    cpy[strlen(cpy) - 1] = '\0';
+    return cpy;
+}
+
+/* Fake history function */
+void add_history(char* unused) {}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#endif
+
+
+int main(int argc, char** argv) {
+
+    puts("Lispy Version 0.0.0.0.1");
+    puts("Press Ctrl+c to Exit\n");
+
+    while(1){
+        char* input = readline("lispy> ");
+        add_history(input);
+        printf("No you're a %s\n", input);
+        free(input);
+    }
+
+    return 0;
+}
